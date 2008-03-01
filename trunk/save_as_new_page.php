eeaa38dc591aefb236b55da0b800893b4cfe7297
lopo <lopo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-03-01 20:05:14 +0000

    Version 3
    git-svn-id: https://plugins.svn.wordpress.org/duplicate-post/tags/0.3@33499 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/save_as_new_page.php
@@ -0,0 +1,28 @@
+<?php 
+	if (!isset( $_GET['post'] ) && !isset( $_POST['post'] )) {
+?>
+<div class="wrap">
+	<p><strong><?php _e('No page to duplicate has been supplied!', DUPLICATE_POST_I18N_DOMAIN) ?></strong></p>
+</div>
+<?php
+	} else {
+		// Get the original template
+		$id = (isset($_GET['post']) ? $_GET['post'] : $_POST['post']);	
+		$post = duplicate_post_get_page($id);
+		
+		// Copy the post and insert it as a template
+		if (isset($post) && $post!=null) {
+			$new_id = duplicate_post_create_duplicate_from_page($post);
+		
+			// Show the post edit
+			echo '<meta content="0; URL=page.php?action=edit&post=' . $new_id . '" http-equiv="Refresh" />';
+			exit;
+		} else {
+?>
+<div class="wrap">
+	<p><strong><?php _e('Post creation failed, could not find original post:', DUPLICATE_POST_I18N_DOMAIN) . ' ' . $id ?></strong></p>
+</div>
+<?php
+		}
+	}
+?>
