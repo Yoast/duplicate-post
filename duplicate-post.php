c59515b1c73ab12efdde914e5e089a3212d46ec9
lopo <lopo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-02 22:31:10 +0000

    Tested on 2.9-beta-2
    git-svn-id: https://plugins.svn.wordpress.org/duplicate-post/tags/0.6.1@178956 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- duplicate-post.php
+++ duplicate-post.php
@@ -3,7 +3,7 @@
 Plugin Name: Duplicate Post
 Plugin URI: http://www.lopo.it/duplicate-post.tar.gz
 Description: Create a copy of a post.
-Version: 0.6
+Version: 0.6.1
 Author: Enrico Battocchi
 Author URI: http://www.lopo.it
 Text Domain: duplicate-post
