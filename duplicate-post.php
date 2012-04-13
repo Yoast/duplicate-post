e99d1652f2453b03e27f60b625b2533a38ab4840
lopo <lopo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-13 09:23:36 +0000

    moved to GPLv2
    git-svn-id: https://plugins.svn.wordpress.org/duplicate-post/trunk@530607 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- duplicate-post.php
+++ duplicate-post.php
@@ -9,11 +9,11 @@
  Text Domain: duplicate-post
  */
 
-/*  Copyright 2009-2011	Enrico Battocchi  (email : enrico.battocchi@gmail.com)
+/*  Copyright 2009-2012	Enrico Battocchi  (email : enrico.battocchi@gmail.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
+the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
