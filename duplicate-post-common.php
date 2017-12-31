d3b5377609668f30a7dd623fa7ef30cc4fd81ce4
lopo <lopo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2017-12-31 10:33:35 +0000

    WordPress coding standards enforced
    git-svn-id: https://plugins.svn.wordpress.org/duplicate-post/trunk@1794819 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- duplicate-post-common.php
+++ duplicate-post-common.php
@@ -1,152 +1,201 @@
 <?php
+/**
+ * Common functions
+ *
+ * @package Duplicate Post
+ * @since 2.0
+ */
 
 /**
- * Test if the user is allowed to copy posts
+ * Tests if the user is allowed to copy posts
+ *
+ * @return bool
  */
 function duplicate_post_is_current_user_allowed_to_copy() {
-	return current_user_can('copy_posts');
+	return current_user_can( 'copy_posts' );
 }
 
 /**
- * Test if post type is enable to be copied
+ * Tests if post type is enable to be copied
+ *
+ * @param string $post_type The post type to check.
+ * @return bool
  */
-function duplicate_post_is_post_type_enabled($post_type){
-	$duplicate_post_types_enabled = get_option('duplicate_post_types_enabled', array ('post', 'page'));
-	if(!is_array($duplicate_post_types_enabled)) $duplicate_post_types_enabled = array($duplicate_post_types_enabled);
-	return in_array($post_type, $duplicate_post_types_enabled);
+function duplicate_post_is_post_type_enabled( $post_type ) {
+	$duplicate_post_types_enabled = get_option( 'duplicate_post_types_enabled', array( 'post', 'page' ) );
+	if ( ! is_array( $duplicate_post_types_enabled ) ) {
+		$duplicate_post_types_enabled = array( $duplicate_post_types_enabled );
+	}
+	return in_array( $post_type, $duplicate_post_types_enabled, true );
 }
 
 /**
  * Wrapper for the option 'duplicate_post_create_user_level'
+ *
+ * @return mixed
  */
 function duplicate_post_get_copy_user_level() {
 	return get_option( 'duplicate_post_copy_user_level' );
 }
 
-// Template tag
 /**
- * Retrieve duplicate post link for post.
+ * Template tag to retrieve/display duplicate post link for post.
  *
- *
- * @param int $id Optional. Post ID.
+ * @param int    $id Optional. Post ID.
  * @param string $context Optional, default to display. How to write the '&', defaults to '&amp;'.
- * @param string $draft Optional, default to true
+ * @param string $draft Optional, default to true.
  * @return string
  */
 function duplicate_post_get_clone_post_link( $id = 0, $context = 'display', $draft = true ) {
-	if ( !duplicate_post_is_current_user_allowed_to_copy() )
+	if ( ! duplicate_post_is_current_user_allowed_to_copy() ) {
 		return;
+	}
 
-	if ( !$post = get_post( $id ) )
+	$post = get_post( $id );
+	if ( ! $post ) {
 		return;
-	
-	if(!duplicate_post_is_post_type_enabled($post->post_type))
+	}
+
+	if ( ! duplicate_post_is_post_type_enabled( $post->post_type ) ) {
 		return;
+	}
 
-	if ($draft)
-	$action_name = "duplicate_post_save_as_new_post_draft";
-	else
-	$action_name = "duplicate_post_save_as_new_post";
+	if ( $draft ) {
+		$action_name = 'duplicate_post_save_as_new_post_draft';
+	} else {
+		$action_name = 'duplicate_post_save_as_new_post';
+	}
 
-	if ( 'display' == $context )
-	$action = '?action='.$action_name.'&amp;post='.$post->ID;
-	else
-	$action = '?action='.$action_name.'&post='.$post->ID;
+	if ( 'display' === $context ) {
+		$action = '?action=' . $action_name . '&amp;post=' . $post->ID;
+	} else {
+		$action = '?action=' . $action_name . '&post=' . $post->ID;
+	}
 
 	$post_type_object = get_post_type_object( $post->post_type );
-	if ( !$post_type_object )
-	return;
+	if ( ! $post_type_object ) {
+		return;
+	}
 
-	return wp_nonce_url(apply_filters( 'duplicate_post_get_clone_post_link', admin_url( "admin.php". $action ), $post->ID, $context ), 'duplicate-post_' . $post->ID);
+	return wp_nonce_url(
+		apply_filters( 'duplicate_post_get_clone_post_link', admin_url( 'admin.php' . $action ), $post->ID, $context ),
+		'duplicate-post_' . $post->ID
+	);
 }
+
 /**
- * Display duplicate post link for post.
+ * Displays duplicate post link for post.
  *
  * @param string $link Optional. Anchor text.
  * @param string $before Optional. Display before edit link.
  * @param string $after Optional. Display after edit link.
- * @param int $id Optional. Post ID.
+ * @param int    $id Optional. Post ID.
  */
 function duplicate_post_clone_post_link( $link = null, $before = '', $after = '', $id = 0 ) {
-	if ( !$post = get_post( $id ) )
-	return;
+	$post = get_post( $id );
+	if ( ! $post ) {
+		return;
+	}
 
-	if ( !$url = duplicate_post_get_clone_post_link( $post->ID ) )
-	return;
+	$url = duplicate_post_get_clone_post_link( $post->ID );
+	if ( ! $url ) {
+		return;
+	}
 
-	if ( null === $link )
-	$link = esc_html__('Copy to a new draft', 'duplicate-post');
+	if ( null === $link ) {
+		$link = esc_html__( 'Copy to a new draft', 'duplicate-post' );
+	}
 
-	$link = '<a class="post-clone-link" href="' . $url . '" title="'
-	. esc_attr__("Copy to a new draft", 'duplicate-post')
-	.'">' . $link . '</a>';
-	echo $before . apply_filters( 'duplicate_post_clone_post_link', $link, $post->ID ) . $after;
+	$link = '<a class="post-clone-link" href="' . $url . '" title="' .
+		esc_attr__( 'Copy to a new draft', 'duplicate-post' ) . '">' . $link . '</a>';
+	echo esc_html( $before . apply_filters( 'duplicate_post_clone_post_link', $link, $post->ID ) . $after );
 }
+
 /**
- * Get original post .
+ * Gets the original post.
  *
- * @param int $post Optional. Post ID or Post object.
+ * @param int    $post Optional. Post ID or Post object.
  * @param string $output Optional, default is Object. Either OBJECT, ARRAY_A, or ARRAY_N.
  * @return mixed Post data
  */
-function duplicate_post_get_original($post = null , $output = OBJECT){
-	if ( !$post = get_post( $post ) )
+function duplicate_post_get_original( $post = null, $output = OBJECT ) {
+	$post = get_post( $post );
+	if ( ! $post ) {
 		return;
-	$original_ID = get_post_meta( $post->ID, '_dp_original');
-	if (empty($original_ID)) return null;
-	$original_post = get_post($original_ID[0],  $output);
+	}
+	$original_id = get_post_meta( $post->ID, '_dp_original' );
+	if ( empty( $original_id ) ) {
+		return null;
+	}
+	$original_post = get_post( $original_id[0], $output );
 	return $original_post;
 }
 
-// Admin bar
+/**
+ * Shows link in the Toolbar.
+ */
 function duplicate_post_admin_bar_render() {
-	if(!is_admin_bar_showing()) return;
+	if ( ! is_admin_bar_showing() ) {
+		return;
+	}
 	global $wp_admin_bar;
 	$current_object = get_queried_object();
-	if ( empty($current_object) )
+	if ( empty( $current_object ) ) {
 		return;
-	if ( ! empty( $current_object->post_type )
-		&& ( $post_type_object = get_post_type_object( $current_object->post_type ) )
-		&& duplicate_post_is_current_user_allowed_to_copy()
-		&& ( $post_type_object->show_ui || 'attachment' == $current_object->post_type )
-		&& (duplicate_post_is_post_type_enabled($current_object->post_type) ) )
-	{
-		$wp_admin_bar->add_menu( array(
-        'id' => 'new_draft',
-        'title' => esc_attr__("Copy to a new draft", 'duplicate-post'),
-        'href' => duplicate_post_get_clone_post_link( $current_object->ID )
-		) );	
 	}
-}
-
-function duplicate_post_add_css() {
-	if(!is_admin_bar_showing()) return;
-	$current_object = get_queried_object();
-	if ( empty($current_object) )
-		return;
-	if ( ! empty( $current_object->post_type )
-		&& ( $post_type_object = get_post_type_object( $current_object->post_type ) )
-		&& duplicate_post_is_current_user_allowed_to_copy()
-		&& ( $post_type_object->show_ui || 'attachment' == $current_object->post_type )
-		&& (duplicate_post_is_post_type_enabled($current_object->post_type) ) )
-	{
-		wp_enqueue_style ( 'duplicate-post', plugins_url('/duplicate-post.css', __FILE__));
-	}
-}
-
-
-add_action('init', 'duplicate_post_init');
-
-function duplicate_post_init(){
-	if (get_option ( 'duplicate_post_show_adminbar' ) == 1) {
-		add_action ( 'wp_before_admin_bar_render', 'duplicate_post_admin_bar_render' );
-		add_action ( 'wp_enqueue_scripts', 'duplicate_post_add_css' );	
+	if ( ! empty( $current_object->post_type ) && ( get_post_type_object( $current_object->post_type ) === $post_type_object ) &&
+		duplicate_post_is_current_user_allowed_to_copy() &&
+		( $post_type_object->show_ui || 'attachment' === $current_object->post_type ) &&
+		( duplicate_post_is_post_type_enabled( $current_object->post_type ) ) ) {
+		$wp_admin_bar->add_menu(
+			array(
+				'id'    => 'new_draft',
+				'title' => esc_attr__( 'Copy to a new draft', 'duplicate-post' ),
+				'href'  => duplicate_post_get_clone_post_link( $current_object->ID ),
+			)
+		);
 	}
 }
 
 /**
- * Sort taxonomy objects: first public, then private
+ * Links stylesheet for Toolbar link.
  */
-function duplicate_post_tax_obj_cmp($a, $b) {
-	return ($a->public < $b->public);
+function duplicate_post_add_css() {
+	if ( ! is_admin_bar_showing() ) {
+		return;
+	}
+	$current_object = get_queried_object();
+	if ( empty( $current_object ) ) {
+		return;
+	}
+	if ( ! empty( $current_object->post_type ) && ( get_post_type_object( $current_object->post_type ) === $post_type_object ) &&
+		duplicate_post_is_current_user_allowed_to_copy() &&
+		( $post_type_object->show_ui || 'attachment' === $current_object->post_type ) &&
+		( duplicate_post_is_post_type_enabled( $current_object->post_type ) ) ) {
+		wp_enqueue_style( 'duplicate-post', plugins_url( '/duplicate-post.css', __FILE__ ) );
+	}
+}
+
+add_action( 'init', 'duplicate_post_init' );
+
+/**
+ * Adds the handlers for displaying link in Toolbar.
+ */
+function duplicate_post_init() {
+	if ( 1 === intval( get_option( 'duplicate_post_show_adminbar' ) ) ) {
+		add_action( 'wp_before_admin_bar_render', 'duplicate_post_admin_bar_render' );
+		add_action( 'wp_enqueue_scripts', 'duplicate_post_add_css' );
+	}
+}
+
+/**
+ * Sorts taxonomy objects: first public, then private.
+ *
+ * @ignore
+ * @param taxonomy $a First taxonomy object.
+ * @param taxonomy $b Second taxonomy object.
+ * @return bool
+ */
+function duplicate_post_tax_obj_cmp( $a, $b ) {
+	return ( $a->public < $b->public );
 }
--- duplicate-post.php
+++ duplicate-post.php
@@ -1,57 +1,72 @@
 <?php
+/**
+ * Plugin Name: Duplicate Post
+ * Plugin URI: https://duplicate-post.lopo.it/
+ * Description: Clone posts and pages.
+ * Version: 3.2.1
+ * Author: Enrico Battocchi
+ * Author URI: https://lopo.it
+ * Text Domain: duplicate-post
+ *
+ * @package Duplicate Post
+ * @since 0.1
+ **/
+
 /*
- Plugin Name: Duplicate Post
- Plugin URI: https://duplicate-post.lopo.it/
- Description: Clone posts and pages.
- Version: 3.2.1
- Author: Enrico Battocchi
- Author URI: https://lopo.it
- Text Domain: duplicate-post
+ * Copyright 2009-2012 Enrico Battocchi (email : enrico.battocchi@gmail.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
-
-/*  Copyright 2009-2012	Enrico Battocchi  (email : enrico.battocchi@gmail.com)
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+	exit();
 }
 
-// Version of the plugin
-define('DUPLICATE_POST_CURRENT_VERSION', '3.2.1' );
-
+define( 'DUPLICATE_POST_CURRENT_VERSION', '3.2.1' );
 
 /**
- * Initialise the internationalisation domain
+ * Initialises the internationalisation domain
  */
 function duplicate_post_load_plugin_textdomain() {
-    load_plugin_textdomain( 'duplicate-post', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+	load_plugin_textdomain( 'duplicate-post', false, basename( dirname( __FILE__ ) ) . '/languages/' );
 }
 add_action( 'plugins_loaded', 'duplicate_post_load_plugin_textdomain' );
 
+add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'duplicate_post_plugin_actions', 10, 4 );
 
-add_filter("plugin_action_links_".plugin_basename(__FILE__), "duplicate_post_plugin_actions", 10, 4);
-
+/**
+ * Adds 'Settings' link to plugin entry in the Plugins list.
+ *
+ * @ignore
+ * @see 'plugin_action_links_$plugin_file'
+ *
+ * @param array  $actions An array of plugin action links.
+ * @param string $plugin_file Path to the plugin file relative to the plugins directory.
+ * @param array  $plugin_data An array of plugin data.
+ * @param string $context The plugin context.
+ * @return array
+ */
 function duplicate_post_plugin_actions( $actions, $plugin_file, $plugin_data, $context ) {
-	array_unshift($actions, "<a href=\"".menu_page_url('duplicatepost', false)."\">".esc_html__("Settings")."</a>");
+	array_unshift(
+		$actions,
+		'<a href="' . menu_page_url( 'duplicatepost', false ) . '">' . esc_html__( 'Settings' ) . '</a>'
+	);
 	return $actions;
 }
 
-require_once (dirname(__FILE__).'/duplicate-post-common.php');
+require_once dirname( __FILE__ ) . '/duplicate-post-common.php';
 
-if (is_admin()){
-	require_once (dirname(__FILE__).'/duplicate-post-admin.php');
+if ( is_admin() ) {
+	include_once dirname( __FILE__ ) . '/duplicate-post-admin.php';
 }
