244d52d46b41a22ec49be130c861ad880956ac17
Enrico Battocchi <enrico.battocchi@gmail.com>
2019-06-28 23:16:28 +0200

    Readme.txt to readme.md

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function( grunt ) {
+'use strict';
+
+	grunt.loadNpmTasks('grunt-wp-readme-to-markdown');
+	
+	grunt.initConfig({
+		  wp_readme_to_markdown: {
+			your_target: {
+			    files: {
+			      'readme.md': 'readme.txt'
+			    },
+			},
+		  },
+		}),
+		
+	grunt.registerTask( 'default', [
+			'wp_readme_to_markdown'
+	]);
+}
