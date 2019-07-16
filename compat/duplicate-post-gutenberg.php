3e443ee83addc2fd49c52585df73c84fc9b5983d
Enrico Battocchi <enrico.battocchi@gmail.com>
2019-07-16 09:03:55 +0200

    PHPDoc comment for Gutenberg/Classic Editor compatibility module

--- compat/duplicate-post-gutenberg.php
+++ compat/duplicate-post-gutenberg.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * Gutenberg (Block editor)/Classic Editor compatibility functions
+ *
+ * @package Duplicate Post
+ * @since 4.0
+ */
 add_filter( 'duplicate_post_get_clone_post_link', 'duplicate_post_classic_editor_clone_link', 10, 4 );
 
 /**
