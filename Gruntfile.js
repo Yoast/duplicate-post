547bcda18f4abe34efc3e14cc18dc4390cce5835
Enrico Battocchi <enrico.battocchi@gmail.com>
2019-08-01 02:56:15 +0200

    New project structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,7 @@
 module.exports = function( grunt ) {
 	'use strict';
 
-	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown')
-	grunt.loadNpmTasks( 'grunt-contrib-compress' );
+	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown');
 
 	grunt.initConfig(
 		{
@@ -10,31 +9,20 @@ module.exports = function( grunt ) {
 			wp_readme_to_markdown: {
 				your_target: {
 					files: {
-						'README.wordpress.md': 'src/readme.txt'
+						'README.wordpress.md': 'readme.txt'
 					},
 				},
 				options: {
 					screenshot_url: 'assets/{screenshot}.jpg'
 				}
 			},
-			compress: {
-				build: {
-					options: {
-						archive: 'dist/<%= pkg.name %>-snapshot.tar.gz',
-					},
-					expand: true,
-					cwd: 'src/',
-					src: ['**/*'],
-					dest: '<%= pkg.name %>/'
-				}
-			}
 		}
-	),
+	);
 
 	grunt.registerTask(
 		'default',
 		[
-			'wp_readme_to_markdown', 'compress:build'
+			'wp_readme_to_markdown'
 		]
 	);
-}
+};
