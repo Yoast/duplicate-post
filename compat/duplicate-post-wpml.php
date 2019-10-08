97aa15fa0497b9cd542a124ba982abcf640eaa2c
Enrico Battocchi <enrico.battocchi@gmail.com>
2019-10-09 00:40:21 +0200

    Missing newline

--- compat/duplicate-post-wpml.php
+++ compat/duplicate-post-wpml.php
@@ -79,8 +79,9 @@ function duplicate_wpml_string_packages() { // phpcs:ignore WordPress.NamingConv
 	global $duplicated_posts;
 
 	foreach ( $duplicated_posts as $original_post_id => $duplicate_post_id ) {
-		$original_string_packages = apply_filters( 'wpml_st_get_post_string_packages', false, $original_post_id ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals
-		$new_string_packages      = apply_filters( 'wpml_st_get_post_string_packages', false, $duplicate_post_id ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals
+
+		$original_string_packages = apply_filters( 'wpml_st_get_post_string_packages', false, $original_post_id );
+		$new_string_packages      = apply_filters( 'wpml_st_get_post_string_packages', false, $duplicate_post_id );
 		if ( is_array( $original_string_packages ) ) {
 			foreach ( $original_string_packages as $original_string_package ) {
 				$translated_original_strings = $original_string_package->get_translated_strings( array() );
