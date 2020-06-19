e99de7f2a5ee925888a51eb8e889d865216e5d1f
Enrico Battocchi <enrico.battocchi@gmail.com>
2020-06-19 16:30:40 +0200

    External CSS for Options page + section headers for no-js display

--- duplicate-post-options.php
+++ duplicate-post-options.php
@@ -46,7 +46,23 @@ function duplicate_post_register_settings() { // whitelist options
 
 
 function duplicate_post_menu() {
-	add_options_page(__("Duplicate Post Options", 'duplicate-post'), __("Duplicate Post", 'duplicate-post'), 'manage_options', 'duplicatepost', 'duplicate_post_options');
+	$page_hook = add_options_page(
+		__( 'Duplicate Post Options', 'duplicate-post' ),
+		__( 'Duplicate Post', 'duplicate-post' ),
+		'manage_options',
+		'duplicatepost',
+		'duplicate_post_options'
+	);
+	add_action( $page_hook, 'duplicate_post_add_options_page_css' );
+}
+
+/**
+ * Enqueues a CSS file with styles for the options page.
+ *
+ * @ignore
+ */
+function duplicate_post_add_options_page_css() {
+	wp_enqueue_style( 'duplicate-post-options', plugins_url( '/duplicate-post-options.css', __FILE__ ), array(), DUPLICATE_POST_CURRENT_VERSION );
 }
 
 function duplicate_post_options() {
@@ -261,52 +277,6 @@ function duplicate_post_options() {
 
 	</script>
 
-	<style>
-header.nav-tab-wrapper {
-	margin: 22px 0 0 0;
-}
-
-header .nav-tab{
-    cursor: pointer;
-}
-
-header .nav-tab:focus {
-	color: #555;
-	box-shadow: none;
-	outline: 1px dotted #000000;
-}
-
-.no-js header.nav-tab-wrapper {
-	display: none;
-}
-
-.no-js section {
-	border-top: 1px dashed #aaa;
-	margin-top: 22px;
-	padding-top: 22px;
-}
-
-.no-js section:first-of-type {
-	margin: 0px;
-	padding: 0px;
-	border: 0px;
-}
-
-.no-js #duplicate_post_settings_form section[hidden] {
-	display: block;
-}
-
-label.taxonomy_private {
-	font-style: italic;
-}
-
-.toggle-private-taxonomies.button-link {
-	font-size: small;
-	margin-top: 1em;
-}
-</style>
-
-
 	<form method="post" action="options.php" style="clear: both" id="duplicate_post_settings_form">
 		<?php settings_fields('duplicate_post_group'); ?>
 
@@ -344,7 +314,7 @@ label.taxonomy_private {
 				role="tabpanel"
 				id="what-tab"
 				aria-labelledby="what">
-
+			<h2 class="hide-if-js"><?php esc_html_e( 'What to copy', 'duplicate-post' ); ?></h2>
 			<table class="form-table" role="presentation">
 				<tr>
 					<th scope="row"><?php esc_html_e('Post/page elements to copy', 'duplicate-post'); ?></th>
@@ -604,6 +574,7 @@ label.taxonomy_private {
 				id="who-tab"
 				aria-labelledby="who"
 				hidden="hidden">
+			<h2 class="hide-if-js"><?php esc_html_e( 'Permissions', 'duplicate-post' ); ?></h2>
 			<table class="form-table" role="presentation">
 				<?php if ( current_user_can( 'promote_users' ) ){ ?>
 				<tr>
@@ -683,6 +654,7 @@ label.taxonomy_private {
 				id="where-tab"
 				aria-labelledby="where"
 				hidden="hidden">
+			<h2 class="hide-if-js"><?php esc_html_e( 'Display', 'duplicate-post' ); ?></h2>
 			<table class="form-table" role="presentation">
                 <tr>
                     <th scope="row"><?php esc_html_e( 'Show links in', 'duplicate-post' ); ?></th>
