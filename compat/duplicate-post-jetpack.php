b98d7b6e97d0d5130decbbc243ec2ccd259518ae
Enrico Battocchi <enrico@yoast.com>
2020-07-07 13:14:00 +0200

    Deprecates duplicate_post_blacklist_filter in favor of duplicate_post_excludelist_filter (#24)
    * Deprecates duplicate_post_blacklist_filter in favor of duplicate_post_excludelist_filter
    
    * Fix: pass args in array do apply_filters_deprecated

--- compat/duplicate-post-jetpack.php
+++ compat/duplicate-post-jetpack.php
@@ -3,20 +3,20 @@ add_action( 'admin_init', 'duplicate_post_jetpack_init' );
 
 
 function duplicate_post_jetpack_init() {
-	add_filter('duplicate_post_blacklist_filter', 'duplicate_post_jetpack_add_to_blacklist', 10, 1 );
-	
+	add_filter('duplicate_post_excludelist_filter', 'duplicate_post_jetpack_add_to_excludelist', 10, 1 );
+
 	if (class_exists('WPCom_Markdown')){
 		add_action('duplicate_post_pre_copy', 'duplicate_post_jetpack_disable_markdown', 10);
 		add_action('duplicate_post_post_copy', 'duplicate_post_jetpack_enable_markdown', 10);
-	}	
+	}
 }
 
-function duplicate_post_jetpack_add_to_blacklist($meta_blacklist) {
+function duplicate_post_jetpack_add_to_excludelist($meta_blacklist) {
 	$meta_blacklist[] = '_wpas*'; //Jetpack Publicize
 	$meta_blacklist[] = '_publicize*'; //Jetpack Publicize
-	
+
 	$meta_blacklist[] = '_jetpack*'; //Jetpack Subscriptions etc.
-	
+
 	return $meta_blacklist;
 }
 
