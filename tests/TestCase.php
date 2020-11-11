d736b69baf1358e84bfd98675343617b50b9a7ca
Andrea Fercia <a.fercia@gmail.com>
2020-11-11 14:16:58 +0100

    Add basic tests infrastructure.

---
+++ tests/TestCase.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Yoast\WP\Duplicate_Post\Tests;
+
+use Brain\Monkey;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+/**
+ * TestCase base class.
+ */
+abstract class TestCase extends BaseTestCase {
+
+	/**
+	 * Test setup.
+	 */
+	protected function setUp() {
+
+		parent::setUp();
+		Monkey\setUp();
+
+		Monkey\Functions\stubs(
+			[
+				// Passing "null" makes the function return its first argument.
+				'esc_attr'       => null,
+				'esc_html'       => null,
+				'esc_textarea'   => null,
+				'__'             => null,
+				'_n'             => function( $single, $plural, $number ) {
+					if ( $number === 1 ) {
+						return $single;
+					}
+
+					return $plural;
+				},
+				'_x'             => null,
+				'esc_html__'     => null,
+				'esc_html_x'     => null,
+				'esc_attr__'     => null,
+				'esc_attr_x'     => null,
+				'esc_url'        => null,
+				'esc_url_raw'    => null,
+				'is_admin'       => false,
+				'is_multisite'   => false,
+				'site_url'       => 'https://www.example.org',
+				'wp_json_encode' => static function( $data, $options = 0, $depth = 512 ) {
+					// phpcs:ignore Yoast.Yoast.AlternativeFunctions -- Mocks the wp_json_encode function.
+					return \json_encode( $data, $options, $depth );
+				},
+				'wp_slash'       => null,
+				'wp_unslash'     => function( $value ) {
+					return \is_string( $value ) ? \stripslashes( $value ) : $value;
+				},
+				'absint'         => static function( $value ) {
+					return \abs( \intval( $value ) );
+				},
+				'wp_parse_args'  => static function ( $settings, $defaults ) {
+					return \array_merge( $defaults, $settings );
+				},
+			]
+		);
+	}
+
+	/**
+	 * Test tear down.
+	 */
+	protected function tearDown() {
+		Monkey\tearDown();
+		parent::tearDown();
+	}
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Duplicate_Post
+ */
+
+define( 'ABSPATH', true );
+define( 'WPSEO_INDEXABLES', true );
+
+define( 'MINUTE_IN_SECONDS', 60 );
+define( 'HOUR_IN_SECONDS', 3600 );
+define( 'DAY_IN_SECONDS', 86400 );
+define( 'WEEK_IN_SECONDS', 604800 );
+define( 'MONTH_IN_SECONDS', 2592000 );
+define( 'YEAR_IN_SECONDS', 31536000 );
+
+if ( function_exists( 'opcache_reset' ) ) {
+	opcache_reset();
+}
+
+if ( file_exists( dirname( __DIR__ ) . '/vendor/autoload.php' ) === false ) {
+	echo PHP_EOL, 'ERROR: Run `composer install` to generate the autoload files before running the unit tests.', PHP_EOL;
+	exit( 1 );
+}
+
+require_once __DIR__ . '/../vendor/autoload.php';
