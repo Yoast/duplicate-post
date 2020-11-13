4dd3f916369984633abd384739b7d7763fb8ae8f
Andrea Fercia <a.fercia@gmail.com>
2020-11-13 15:20:44 +0100

    Try publish_post and publish_page hooks.

--- src/class-duplicate-post-republish.php
+++ src/class-duplicate-post-republish.php
@@ -25,49 +25,51 @@ class Duplicate_Post_Republish {
 	 * @return void
 	 */
 	private function register_hooks() {
-		\add_action( 'post_updated', array( $this, 'duplicate_post_republish' ), 10, 3 );
+		\add_action( 'publish_post', array( $this, 'duplicate_post_republish' ), 10, 2 );
+		\add_action( 'publish_page', array( $this, 'duplicate_post_republish' ), 10, 2 );
 	}
 
 	/**
 	 * Handles the republishing flow.
 	 *
-	 * @param int      $post_ID     Post ID.
-	 * @param \WP_Post $post_after  Post object following the update.
-	 * @param \WP_Post $post_before Post object before the update.
+	 * @param int      $post_id The post ID.
+	 * @param \WP_Post $post    The post object.
 	 *
 	 * @return void
 	 */
-	public function duplicate_post_republish( $post_ID, $post_after, $post_before ) {
-		$this->republish_post_elements( $post_ID, $post_after, $post_before );
+	public function duplicate_post_republish( $post_id, $post ) {
+		// Runs also on: trash, restore, pending review...
+		$this->republish_post_elements( $post_id, $post );
 	}
 
 	/**
 	 * Republishes the post elements overwriting the original post.
 	 *
-	 * @param int      $post_copy_id     Post ID.
-	 * @param \WP_Post $post_copy_after  Post object following the update.
-	 * @param \WP_Post $post_copy_before Post object before the update.
+	 * @param int      $post_copy_id Post ID.
+	 * @param \WP_Post $post_copy    Post object.
 	 *
 	 * @return void
 	 */
-	private function republish_post_elements( $post_copy_id, $post_copy_after, $post_copy_before ) {
+	private function republish_post_elements( $post_copy_id, $post_copy ) {
 
 		$original_post_id = \get_post_meta( $post_copy_id, '_dp_is_rewrite_republish_copy', true );
+
 		if ( ! $original_post_id ) {
 			return;
 		}
 
-		$post              = $post_copy_after;
-		$post->ID          = $original_post_id;
-		$post->post_status = 'publish';
+		$post_to_be_rewritten            = $post_copy;
+		$post_to_be_rewritten->ID        = $original_post_id;
+		$post_to_be_rewritten->post_name = get_post_field( 'post_name', $post_to_be_rewritten->ID );
 
-		$rewritten_post_id = \wp_update_post( \wp_slash( (array) $post ), true );
+		$rewritten_post_id = \wp_update_post( \wp_slash( (array) $post_to_be_rewritten ), true );
 
 		if ( 0 === $rewritten_post_id || is_wp_error( $rewritten_post_id ) ) {
 			// Error handling here.
 			die( 'An error occurred.' );
 		}
 
+		// Deleting the copy bypassing the trash also deletes the post copy meta.
 		\wp_delete_post( $post_copy_id, true );
 
 		// Add nonce verification here.
@@ -75,9 +77,8 @@ class Duplicate_Post_Republish {
 			\add_query_arg(
 				array(
 					'republished' => 1,
-					'ids'         => $post->ID,
 				),
-				\admin_url( 'post.php?action=edit&post=' . $post->ID . ( isset( $_GET['classic-editor'] ) ? '&classic-editor' : '' ) )
+				\admin_url( 'post.php?action=edit&post=' . $original_post_id )
 			)
 		);
 		exit();
