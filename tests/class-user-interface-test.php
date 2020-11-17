b2c430364a7aa38c12495f7c164ee8e7a6067b85
Enrico Battocchi <enrico.battocchi@gmail.com>
2020-11-17 16:43:40 +0100

    Add some simple tests.

--- tests/class-user-interface-test.php
+++ tests/class-user-interface-test.php
@@ -39,4 +39,89 @@ class User_Interface_Test extends TestCase {
 		$this->handler  = Mockery::mock( Handler::class );
 		$this->instance = new User_Interface( $this->handler );
 	}
+
+	/**
+	 * Tests the registration of the hooks.
+	 *
+	 * @covers ::register_hooks
+	 */
+	public function test_register_hooks() {
+		$this->instance->register_hooks();
+
+		$this->assertNotFalse( \has_action( 'enqueue_block_editor_assets', [ $this->instance, 'enqueue_block_editor_scripts' ] ), 'Does not have expected enqueue_block_editor_assets action' );
+		$this->assertNotFalse( \has_action( 'admin_enqueue_scripts', [ $this->instance, 'should_previously_used_keyword_assessment_run' ] ), 'Does not have expected admin_enqueue_scripts action' );
+		$this->assertNotFalse( \has_action( 'admin_init', [ $this->instance, 'add_bulk_filters' ] ), 'Does not have expected admin_init action' );
+		$this->assertNotFalse( \has_action( 'post_submitbox_start', [ $this->instance, 'add_rewrite_and_republish_post_button' ] ), 'Does not have expected post_submitbox_start action' );
+		$this->assertNotFalse( \has_filter( 'removable_query_args', [ $this->instance, 'add_removable_query_args' ] ), 'Does not have expected removable_query_args filter' );
+		$this->assertNotFalse( \has_action( 'admin_notices', [ $this->instance, 'single_action_admin_notice' ] ), 'Does not have expected admin_notices action' );
+		$this->assertNotFalse( \has_action( 'admin_notices', [ $this->instance, 'bulk_action_admin_notice' ] ), 'Does not have expected admin_notices action' );
+		$this->assertNotFalse( \has_action( 'wp_before_admin_bar_render', [ $this->instance, 'admin_bar_render' ] ), 'Does not have expected wp_before_admin_bar_render action' );
+	}
+
+	public function test_add_removable_query_args() {
+		$array = [
+			'activate',
+			'activated',
+			'admin_email_remind_later',
+			'approved',
+			'deactivate',
+			'delete_count',
+			'deleted',
+			'disabled',
+			'doing_wp_cron',
+			'enabled',
+			'error',
+			'hotkeys_highlight_first',
+			'hotkeys_highlight_last',
+			'locked',
+			'message',
+			'same',
+			'saved',
+			'settings-updated',
+			'skipped',
+			'spammed',
+			'trashed',
+			'unspammed',
+			'untrashed',
+			'update',
+			'updated',
+			'wp-post-new-reload',
+			'cloned',
+		];
+
+		$this->assertEquals(
+			[
+				'activate',
+				'activated',
+				'admin_email_remind_later',
+				'approved',
+				'deactivate',
+				'delete_count',
+				'deleted',
+				'disabled',
+				'doing_wp_cron',
+				'enabled',
+				'error',
+				'hotkeys_highlight_first',
+				'hotkeys_highlight_last',
+				'locked',
+				'message',
+				'same',
+				'saved',
+				'settings-updated',
+				'skipped',
+				'spammed',
+				'trashed',
+				'unspammed',
+				'untrashed',
+				'update',
+				'updated',
+				'wp-post-new-reload',
+				'cloned',
+				'rewriting',
+				'bulk_rewriting'
+			],
+			$this->instance->add_removable_query_args( $array )
+		);
+	}
 }
--- tests/class-utils-test.php
+++ tests/class-utils-test.php
@@ -33,13 +33,44 @@ class Utils_Test extends TestCase {
 	 * @return array
 	 */
 	public function flatten_version_provider() {
-		return array(
-			array( '3.0', '300' ),
-			array( '1.4', '140' ),
-			array( '', '' ),
-			array( '3.0.0', '300' ),
-			array( '25.1456.140', '251456140' ),
-			array( '1', '1' ),
-		);
+		return [
+			[ '3.0', '300' ],
+			[ '1.4', '140' ],
+			[ '', '' ],
+			[ '3.0.0', '300' ],
+			[ '25.1456.140', '251456140' ],
+			[ '1', '1' ],
+		];
+	}
+
+	/**
+	 * Tests the addslashes_to_strings_only function.
+	 *
+	 * @covers       Utils::addslashes_to_strings_only
+	 * @dataProvider addslashes_to_strings_only_provider
+	 *
+	 * @param mixed $original Input value.
+	 * @param mixed $expected Expected output.
+	 */
+	public function test_addslashes_to_strings_only( $original, $expected ) {
+		$this->assertEquals( $expected, Utils::addslashes_to_strings_only( $original ) );
+	}
+
+	/**
+	 * Data provider for test_addslashes_to_strings_only.
+	 *
+	 * @return array
+	 */
+	public function addslashes_to_strings_only_provider() {
+		return [
+			[ "O'Reilly", "O\'Reilly" ],
+			[ 'A string with "quotes"', 'A string with \"quotes\"' ],
+			[ 'C:\\Windows', 'C:\\\\Windows' ],
+			[ 'A string with NUL \0 in the middle', 'A string with NUL \\\0 in the middle' ],
+			[ 1000, 1000 ],
+			[ 999.99, 999.99 ],
+			[ true , true ],
+			[ [ 'must', 'remain', 'array'] , [ 'must', 'remain', 'array'] ],
+		];
 	}
 }
