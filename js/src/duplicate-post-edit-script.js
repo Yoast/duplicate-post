9b44fbd3ff7bd6e7c10e1351c17ebbfdc1addf6e
Enrico Battocchi <enrico.battocchi@gmail.com>
2020-11-18 13:55:15 +0100

    Address first round of CR (JS file)

--- js/src/duplicate-post-edit-script.js
+++ js/src/duplicate-post-edit-script.js
@@ -20,9 +20,9 @@ registerPlugin( 'duplicate-post', {
 	render
 } );
 
-if ( parseInt( duplicatePostRewriteRepost.rewriting) ) {
-	(function (wp) {
-		wp.data.dispatch('core/notices').createNotice(
+if ( parseInt( duplicatePostRewriteRepost.rewriting ) ) {
+	( function( wp ) {
+		wp.data.dispatch( 'core/notices' ).createNotice(
 			'warning',
 			__(
 				'You can now start rewriting your post in this duplicate of the original post. If you click "Republish", this rewritten post will replace the original post.',
@@ -32,5 +32,5 @@ if ( parseInt( duplicatePostRewriteRepost.rewriting) ) {
 				isDismissible: true, // Whether the user can dismiss the notice.
 			}
 		);
-	})(window.wp);
+	} )( window.wp );
 }
