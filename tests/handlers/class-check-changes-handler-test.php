44b69c49be6a02b06a3d78cc02c356c932deccf4
Enrico Battocchi <enrico.battocchi@gmail.com>
2020-12-11 19:12:43 +0100

    Complete test suite for new handler

--- tests/handlers/class-check-changes-handler-test.php
+++ tests/handlers/class-check-changes-handler-test.php
@@ -64,13 +64,13 @@ class Check_Changes_Handler_Test extends TestCase {
 	}
 
 	/**
-	 * Tests the check_changes_action_handler function.
+	 * Tests the successful check_changes_action_handler function.
 	 *
 	 * @covers \Yoast\WP\Duplicate_Post\Handlers\Check_Changes_Handler::check_changes_action_handler
 	 * @runInSeparateProcess
 	 * @preserveGlobalState disabled
 	 */
-	public function test_check_changes_action_handler() {
+	public function test_check_changes_action_handler_successful() {
 		$utils                  = \Mockery::mock( 'alias:\Yoast\WP\Duplicate_Post\Utils' );
 		$_GET['post']           = '123';
 		$_REQUEST['action']     = 'duplicate_post_check_changes';
@@ -85,7 +85,7 @@ class Check_Changes_Handler_Test extends TestCase {
 		$original->post_content = 'Original content';
 		$original->post_excerpt = 'Original excerpt';
 		$post_link              = 'https://yoa.st/wp-admin/post.php?id=123';
-		$original_link          = 'https://yoa.st/wp-admin/post.php?id=100';
+		$original_link          = '<a href="https://yoa.st/wp-admin/post.php?id=100">Unchanged Title</a>';
 
 		Monkey\Functions\expect( '\check_admin_referer' )
 			->with( 'duplicate_post_check_changes_123' );
@@ -110,17 +110,97 @@ class Check_Changes_Handler_Test extends TestCase {
 
 		Monkey\Functions\expect( '\wp_text_diff' )
 			->with( $original->post_title, $post->post_title )
-			->andReturn( null );
-
-		Monkey\Functions\expect( '\wp_text_diff' )
-			->with( $original->post_content, $post->post_content )
-			->andReturn( 'diff-title' );
-
-		Monkey\Functions\expect( '\wp_text_diff' )
-			->with( $original->post_excerpt, $post->post_excerpt )
-			->andReturn( 'diff-title' );
+			->andReturn( null, 'diff-content', 'diff-excerpt' );
 
 		$this->instance->expects( 'require_wordpress_footer' );
+
+		$this->expectOutputString(
+			'		<div class="wrap">
+			<h1 class="long-header">
+			Compare changes of duplicated post with the original (<a href="https://yoa.st/wp-admin/post.php?id=100">Unchanged Title</a>)				</h1>
+			<a href="https://yoa.st/wp-admin/post.php?id=123"></a>
+			<div class="revisions">
+				<div class="revisions-control-frame">
+					<div class="revisions-controls"></div>
+				</div>
+				<div class="revisions-diff-frame">
+					<div class="revisions-diff">
+						<div class="diff">
+														<h3>Title</h3>
+								<table class="diff"><colgroup><col class="content diffsplit left"><col class="content diffsplit middle"><col class="content diffsplit right"></colgroup><tbody><tr><td>Unchanged Title</td><td></td><td>Unchanged Title</td></tr></tbody></table>								<h3>Content</h3>
+								diff-content								<h3>Excerpt</h3>
+								diff-excerpt
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		'
+		);
+		$this->instance->check_changes_action_handler();
+	}
+
+	/**
+	 * Tests the check_changes_action_handler function when no ID has been passed in the query.
+	 *
+	 * @covers \Yoast\WP\Duplicate_Post\Handlers\Check_Changes_Handler::check_changes_action_handler
+	 */
+	public function test_check_changes_action_handler_no_id_in_query() {
+		Monkey\Functions\expect( '\wp_die' )
+			->with( 'No post has been supplied!' );
+
+		$this->instance->check_changes_action_handler();
+	}
+
+
+	/**
+	 * Tests the check_changes_action_handler function when there is no post.
+	 *
+	 * @covers \Yoast\WP\Duplicate_Post\Handlers\Check_Changes_Handler::check_changes_action_handler
+	 */
+	public function test_check_changes_action_handler_no_post() {
+		$_GET['post']       = '123';
+		$_REQUEST['action'] = 'duplicate_post_check_changes';
+
+		Monkey\Functions\expect( '\check_admin_referer' )
+			->with( 'duplicate_post_check_changes_123' );
+
+		Monkey\Functions\expect( '\get_post' )
+			->with( 123 )
+			->andReturnNull();
+
+		Monkey\Functions\expect( '\wp_die' )
+			->with( 'Changes overview failed, could not find post with ID 123.' );
+
+		$this->instance->check_changes_action_handler();
+	}
+
+	/**
+	 * Tests the check_changes_action_handler function when there is no original post.
+	 *
+	 * @covers \Yoast\WP\Duplicate_Post\Handlers\Check_Changes_Handler::check_changes_action_handler
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test_check_changes_action_handler_no_original() {
+		$utils              = \Mockery::mock( 'alias:\Yoast\WP\Duplicate_Post\Utils' );
+		$_GET['post']       = '123';
+		$_REQUEST['action'] = 'duplicate_post_check_changes';
+		$post               = \Mockery::mock( \WP_Post::class );
+
+		Monkey\Functions\expect( '\check_admin_referer' )
+			->with( 'duplicate_post_check_changes_123' );
+
+		Monkey\Functions\expect( '\get_post' )
+			->with( 123 )
+			->andReturn( $post );
+
+		$utils->expects( 'get_original' )
+			->with( $post )
+			->andReturnNull();
+
+		Monkey\Functions\expect( '\wp_die' )
+			->with( 'Changes overview failed, could not find original post.' );
 
 		$this->instance->check_changes_action_handler();
 	}
