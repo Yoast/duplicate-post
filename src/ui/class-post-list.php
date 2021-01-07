a2c36ea2ffabde0d4332dfbd55e4ec485f8b9f56
Jimmy Comack <jimmy@yoast.com>
2021-01-07 16:24:46 +0100

    Optimized the query and fixed post count

--- src/ui/class-post-list.php
+++ src/ui/class-post-list.php
@@ -22,6 +22,13 @@ class Post_List {
 	protected $permissions_helper;
 
 	/**
+	 * Holds the array of copy IDs.
+	 *
+	 * @var array
+	 */
+	protected $copy_ids;
+
+	/**
 	 * Initializes the class.
 	 *
 	 * @param Permissions_Helper $permissions_helper The Permissions helper object.
@@ -36,7 +43,8 @@ class Post_List {
 	 * @return void
 	 */
 	public function register_hooks() {
-		\add_filter( 'parse_query', [ $this, 'filter_rewrite_and_republish_copies' ], 10 );
+		\add_filter( 'parse_query', [ $this, 'filter_rewrite_and_republish_copies' ] );
+		\add_filter( 'wp_count_posts', [ $this, 'filter_rewrite_and_republish_counts' ] );
 	}
 
 	/**
@@ -47,26 +55,64 @@ class Post_List {
 	 * @return \WP_Query The updated post WordPress query.
 	 */
 	public function filter_rewrite_and_republish_copies( \WP_Query $query ) {
-		if ( ! \is_admin() ) {
+		if ( ! $this->should_filter() ) {
 			return $query;
 		}
 
-		$current_screen = \get_current_screen();
-		if ( $current_screen->base !== 'edit' ) {
-			return $query;
-		}
+		$post_not_in = $query->get( 'post__not_in', [] );
+		$post_not_in = array_merge( $post_not_in, $this->get_copy_ids() );
 
-		if ( $this->permissions_helper->is_elementor_active() ) {
-			$query->set(
-				'meta_query',
-				[
-					[
-						'key'     => '_dp_is_rewrite_republish_copy',
-						'compare' => 'NOT EXISTS',
-					],
-				]
-			);
-		}
+		$query->set( 'post__not_in', $post_not_in );
+
 		return $query;
+	}
+
+	/**
+	 * Filters out the Rewrite and Republish posts from the post counts.
+	 *
+	 * @param object $counts The current post counts.
+	 *
+	 * @return object The updated post counts.
+	 */
+	public function filter_rewrite_and_republish_counts( $counts ) {
+		if ( ! $this->should_filter() ) {
+			return $counts;
+		}
+
+		$counts->draft = $counts->draft - count( $this->get_copy_ids() );
+
+		return $counts;
+	}
+
+	/**
+	 * Queries the database to get the IDs of all Rewrite and Republish copies.
+	 *
+	 * @return array The IDs of the copies.
+	 */
+	protected function get_copy_ids() {
+		global $wpdb;
+
+		if ( ! is_null( $this->copy_ids ) ) {
+			return $this->copy_ids;
+		}
+
+		$query = $wpdb->prepare(
+			'SELECT post_id FROM ' . $wpdb->postmeta . ' WHERE meta_key = %s', '_dp_is_rewrite_republish_copy'
+		);
+
+		$this->copy_ids = $wpdb->get_col( $query );
+
+		return $this->copy_ids;
+	}
+
+	/**
+	 * Determines whether the filter should be applied.
+	 *
+	 * @return bool Whether the filter should be applied.
+	 */
+	protected function should_filter() {
+		$current_screen = \get_current_screen();
+
+		return ( \is_admin() && $current_screen->base === 'edit' && $this->permissions_helper->is_elementor_active() );
 	}
 }
