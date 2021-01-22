0e219970bec1319cd57a308d4aa6caf5f3be4c8b
Enrico Battocchi <enrico.battocchi@gmail.com>
2021-01-22 14:31:41 +0100

    Hide the post status changer in Elementor instead of removing it

--- src/ui/class-block-editor.php
+++ src/ui/class-block-editor.php
@@ -7,7 +7,6 @@
 
 namespace Yoast\WP\Duplicate_Post\UI;
 
-use Elementor\Core\Base\Document;
 use WP_Post;
 use Yoast\WP\Duplicate_Post\Permissions_Helper;
 use Yoast\WP\Duplicate_Post\Utils;
@@ -57,7 +56,7 @@ class Block_Editor {
 	 * @return void
 	 */
 	public function register_hooks() {
-		\add_action( 'elementor/documents/register_controls', [ $this, 'remove_elementor_post_status' ] );
+		\add_action( 'elementor/editor/after_enqueue_styles', [ $this, 'hide_elementor_post_status' ] );
 		\add_action( 'elementor/editor/before_enqueue_scripts', [ $this, 'enqueue_elementor_script' ], 9 );
 		\add_action( 'admin_enqueue_scripts', [ $this, 'should_previously_used_keyword_assessment_run' ], 9 );
 		\add_action( 'enqueue_block_editor_assets', [ $this, 'enqueue_block_editor_scripts' ] );
@@ -81,20 +80,20 @@ class Block_Editor {
 	}
 
 	/**
-	 * Removes the post status control if we're working on a Rewrite and Republish post.
-	 *
-	 * @param Document $document The document to remove the control from.
+	 * Hides the post status control if we're working on a Rewrite and Republish post.
 	 *
 	 * @return void
 	 */
-	public function remove_elementor_post_status( $document ) {
+	public function hide_elementor_post_status() {
 		$post = \get_post();
 
 		if ( ! $post instanceof WP_Post || ! $this->permissions_helper->is_rewrite_and_republish_copy( $post ) ) {
 			return;
 		}
-
-		$document->remove_control( 'post_status' );
+		\wp_add_inline_style(
+			'elementor-editor',
+			'.elementor-control-post_status { display: none !important; }'
+		);
 	}
 
 	/**
