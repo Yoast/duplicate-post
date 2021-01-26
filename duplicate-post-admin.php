7265cc084c0aa2de182ad09da8205458df4f9cc8
Enrico Battocchi <enrico.battocchi@gmail.com>
2021-01-26 10:21:59 +0100

    Fix hidden field in Mailchimp form for RTL languages - compatibile with most browsers

--- duplicate-post-admin.php
+++ duplicate-post-admin.php
@@ -807,7 +807,7 @@ function duplicate_post_newsletter_signup_form() {
 		<div class="response" id="mce-error-response" style="display:none"></div>
 		<div class="response" id="mce-success-response" style="display:none"></div>
 	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-	<div style="position: absolute; inset-inline-start: -5000px;" aria-hidden="true"><input type="text" name="b_ffa93edfe21752c921f860358_972f1c9122" tabindex="-1" value=""></div>
+	<div class="screen-reader-text" aria-hidden="true"><input type="text" name="b_ffa93edfe21752c921f860358_972f1c9122" tabindex="-1" value=""></div>
 	</div>
 </form>
 </div>
