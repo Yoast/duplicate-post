42917e6a0c45db4f608e0573c98b8ddf456c0739
petaryoast <86779744+petaryoast@users.noreply.github.com>
2021-10-22 10:25:18 +0200

    Split form handling and output

--- src/ui/newsletter.php
+++ src/ui/newsletter.php
@@ -16,7 +16,7 @@ class Newsletter {
 	 *
 	 * @return void.
 	 */
-	public function newsletter_subscribe_to_mailblue($url, $email) {
+	public static function newsletter_subscribe_to_mailblue($url, $email) {
 		$response = wp_remote_post(
 			$url,
 			[
@@ -39,6 +39,10 @@ class Newsletter {
 	 * @return string The HTML of the newsletter signup form (escaped).
 	 */
 	public static function newsletter_signup_form() {
+
+		self::newsletter_handle_form();
+
+
 		$copy = sprintf(
 		/* translators: 1: Yoast */
 			esc_html__(
@@ -50,34 +54,42 @@ class Newsletter {
 
 		$email_label = esc_html__( 'Email Address', 'duplicate-post' );
 
-		//https://staging-platform-my.yoast.com/api/Mailing-list/subscribe
-
+		//3. form output
 		$html = '
-<!-- Begin Mailchimp Signup Form -->
-<div id="mc_embed_signup">
-<form method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-	<div id="mc_embed_signup_scroll">
-	' . $copy . '
-<div class="mc-field-group" style="margin-top: 8px;">
-	<label for="mce-EMAIL">' . $email_label . '</label>
-	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
-	<input type="submit" value="' . esc_attr__( 'Subscribe', 'duplicate-post' ) . '" name="subscribe" id="mc-embedded-subscribe" class="button">
-</div>
-	<div id="mce-responses" class="clear">
-		<div class="response" id="mce-error-response" style="display:none"></div>
-		<div class="response" id="mce-success-response" style="display:none"></div>
-	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-	<div class="screen-reader-text" aria-hidden="true"><input type="text" name="b_ffa93edfe21752c921f860358_972f1c9122" tabindex="-1" value=""></div>
-	</div>
-</form>
-</div>
-<!--End mc_embed_signup-->
-';
-
-		if(isset($_POST['EMAIL']) && $_POST['EMAIL'] !== '') {
-			call_user_func(__NAMESPACE__ . 'Newsletter::newsletter_subscribe_to_mailblue','https://staging-platform-my.yoast.com/api/Mailing-list/subscribe', $_POST['EMAIL']);
-		}
+		<!-- Begin Newsletter Signup Form -->
+		<div>
+		<form  method="post" id="newsletter-subscribe-form" name="newsletter-subscribe-form" class="validate" novalidate>
+			<p>' . $copy . '</p>
+		<div class="newsletter-field-group" style="margin-top: 8px;">
+			<label for="newsletter-EMAIL" style="display: block; margin-bottom: 8px;"><strong>' . $email_label . '</strong></label>
+			<input type="email" value="" name="EMAIL" class="required email" id="newsletter-EMAIL">
+			<input type="submit" value="' . esc_attr__( 'Subscribe', 'duplicate-post' ) . '" name="subscribe" id="newsletter-subscribe" class="button">
+		</div>
+			<div id="newsletter-responses" class="clear">
+				<div class="newsletter-response" id="newsletter-error-response" style="display: none;"></div>
+				<div class="newsletter-response" id="newsletter-success-response" style="display: none;"></div>
+			</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+			<div class="screen-reader-text" aria-hidden="true"><input type="text" name="b_ffa93edfe21752c921f860358_972f1c9122" tabindex="-1" value=""></div>
+		</form>
+		</div>
+		<!--End Newsletter Signup Form-->
+		';
 
 		return $html;
+	}
+
+	/**
+	 * Handles and validates Newsletter form.
+	 */
+	private static function newsletter_handle_form() {
+		if (! isset($_POST['EMAIL']) || $_POST['EMAIL'] === '') {
+			return;
+		}
+
+		if (! is_email($_POST['EMAIL'])) {
+			return;
+		}
+
+		self::newsletter_subscribe_to_mailblue('https://staging-platform-my.yoast.com/api/Mailing-list/subscribe', $_POST['EMAIL']);
 	}
 }
