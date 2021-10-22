b045d7caf97a56f793dfd5cc4d2349f3548d9db0
petaryoast <86779744+petaryoast@users.noreply.github.com>
2021-10-22 11:20:26 +0200

    Add form submit feedback

--- src/ui/newsletter.php
+++ src/ui/newsletter.php
@@ -9,14 +9,14 @@ class Newsletter {
 	}
 
 	/**
-	 * Handles subscription request
+	 * Handles subscription request.
 	 *
 	 * @param $url API endpoint.
 	 * @param $email Subscriber email.
 	 *
-	 * @return void.
+	 * @return array Feedback response.
 	 */
-	public static function newsletter_subscribe_to_mailblue($url, $email) {
+	private static function newsletter_subscribe_to_mailblue($url, $email) {
 		$response = wp_remote_post(
 			$url,
 			[
@@ -30,7 +30,19 @@ class Newsletter {
 			]
 		);
 
-		print_r(wp_remote_retrieve_body($response));
+		$wp_remote_retrieve_response_code = wp_remote_retrieve_response_code($response);
+
+		if($wp_remote_retrieve_response_code < 201 || $wp_remote_retrieve_response_code >= 300) {
+			return [
+				'status'	=> 'error',
+				'message'	=> esc_html__('Something went wrong. Please try again later.', 'duplicate-post'),
+			];
+		}
+
+		return [
+			'status'	=> 'success',
+			'message'	=> esc_html__('You have successfully subscribed to the newsletter. Please check your inbox to confirm.', 'duplicate-post'),
+		];
 	}
 
 	/**
@@ -40,8 +52,18 @@ class Newsletter {
 	 */
 	public static function newsletter_signup_form() {
 
-		self::newsletter_handle_form();
+		$newsletter_form_response = self::newsletter_handle_form();
 
+		$response_html = '';
+		if(is_array($newsletter_form_response)) {
+			$response_message = $newsletter_form_response['message'];
+
+			$response_html = '
+			<div id="newsletter-responses" class="clear">
+				<div class="newsletter-response" id="newsletter-response">' . $response_message . '</div>
+			</div>
+			';
+		}
 
 		$copy = sprintf(
 		/* translators: 1: Yoast */
@@ -54,42 +76,42 @@ class Newsletter {
 
 		$email_label = esc_html__( 'Email Address', 'duplicate-post' );
 
-		//3. form output
 		$html = '
 		<!-- Begin Newsletter Signup Form -->
 		<div>
 		<form  method="post" id="newsletter-subscribe-form" name="newsletter-subscribe-form" class="validate" novalidate>
-			<p>' . $copy . '</p>
+		<p>' . $copy . '</p>
 		<div class="newsletter-field-group" style="margin-top: 8px;">
-			<label for="newsletter-EMAIL" style="display: block; margin-bottom: 8px;"><strong>' . $email_label . '</strong></label>
-			<input type="email" value="" name="EMAIL" class="required email" id="newsletter-EMAIL">
+			<label for="newsletter-email" style="display: block; margin-bottom: 8px;"><strong>' . $email_label . '</strong></label>
+			<input type="email" value="" name="EMAIL" class="required email" id="newsletter-email">
 			<input type="submit" value="' . esc_attr__( 'Subscribe', 'duplicate-post' ) . '" name="subscribe" id="newsletter-subscribe" class="button">
 		</div>
-			<div id="newsletter-responses" class="clear">
-				<div class="newsletter-response" id="newsletter-error-response" style="display: none;"></div>
-				<div class="newsletter-response" id="newsletter-success-response" style="display: none;"></div>
-			</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+		' . $response_html . '
 			<div class="screen-reader-text" aria-hidden="true"><input type="text" name="b_ffa93edfe21752c921f860358_972f1c9122" tabindex="-1" value=""></div>
 		</form>
 		</div>
 		<!--End Newsletter Signup Form-->
 		';
 
+
+
 		return $html;
 	}
 
 	/**
 	 * Handles and validates Newsletter form.
+	 *
+	 * @returns null|array
 	 */
 	private static function newsletter_handle_form() {
 		if (! isset($_POST['EMAIL']) || $_POST['EMAIL'] === '') {
-			return;
+			return null;
 		}
 
 		if (! is_email($_POST['EMAIL'])) {
-			return;
+			return null;
 		}
 
-		self::newsletter_subscribe_to_mailblue('https://staging-platform-my.yoast.com/api/Mailing-list/subscribe', $_POST['EMAIL']);
+		return self::newsletter_subscribe_to_mailblue('https://staging-platform-my.yoast.com/api/Mailing-list/subscribe', $_POST['EMAIL']);
 	}
 }
