bb540cd128dfe304fce44898ec92959efc28d934
petaryoast <86779744+petaryoast@users.noreply.github.com>
2021-10-22 14:38:01 +0200

    Implement live API link and refine styling

--- src/ui/newsletter.php
+++ src/ui/newsletter.php
@@ -31,25 +31,23 @@ class Newsletter {
 
 		$response_html = '';
 		if(is_array($newsletter_form_response)) {
+			$response_status = $newsletter_form_response['status'];
 			$response_message = $newsletter_form_response['message'];
 
-			$response_html = '<div class="newsletter-response clear" id="newsletter-response" style="margin-top: 6px;">' . $response_message . '</div>';
+			$response_html = '<div class="newsletter-response-' . $response_status . ' clear" id="newsletter-response" style="margin-top: 6px;">' . $response_message . '</div>';
 		}
 
 		$html = '
 		<!-- Begin Newsletter Signup Form -->
-		<div>
-		<form  method="post" id="newsletter-subscribe-form" name="newsletter-subscribe-form" class="validate" novalidate>
+		<form method="post" id="newsletter-subscribe-form" name="newsletter-subscribe-form" novalidate>
 		<p>' . $copy . '</p>
-		<div class="newsletter-field-group" style="margin-top: 8px;">
-			<label for="newsletter-email" style="display: block; margin-bottom: 8px;"><strong>' . $email_label . '</strong></label>
-			<input type="email" value="" name="EMAIL" class="required email" id="newsletter-email">
+		<div class="newsletter-field-group" style="display: flex; align-items: center;">
+			<label for="newsletter-email" style="margin-right: 4px;"><strong>' . $email_label . '</strong></label>
+			<input type="email" value="" name="EMAIL" class="required email" id="newsletter-email" style="margin-right: 4px;">
 			<input type="submit" value="' . esc_attr__( 'Subscribe', 'duplicate-post' ) . '" name="subscribe" id="newsletter-subscribe" class="button">
 		</div>
 		' . $response_html . '
-			<div class="screen-reader-text" aria-hidden="true"><input type="text" name="b_ffa93edfe21752c921f860358_972f1c9122" tabindex="-1" value=""></div>
 		</form>
-		</div>
 		<!--End Newsletter Signup Form-->
 		';
 
@@ -73,27 +71,26 @@ class Newsletter {
 			];
 		}
 
-		return self::newsletter_subscribe_to_mailblue('https://staging-platform-my.yoast.com/api/Mailing-list/subscribe', $_POST['EMAIL']);
+		return self::newsletter_subscribe_to_mailblue($_POST['EMAIL']);
 	}
 
 	/**
-	 * Handles subscription request.
+	 * Handles subscription request and provides feedback response.
 	 *
-	 * @param $url API endpoint.
 	 * @param $email Subscriber email.
 	 *
 	 * @return array Feedback response.
 	 */
-	private static function newsletter_subscribe_to_mailblue($url, $email) {
+	private static function newsletter_subscribe_to_mailblue($email) {
 		$response = wp_remote_post(
-			$url,
+			'https://my.yoast.com/api/Mailing-list/subscribe',
 			[
 				'method'      => 'POST',
 				'body'        => [
 					'customerDetails' => [
 						'email' => $email,
 					],
-					'list'			 => 'Newsletter staging test'
+					'list'			 => 'Yoast newsletter',
 				],
 			]
 		);
