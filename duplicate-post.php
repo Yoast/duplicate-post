ad76229915860077cb50581bc11fa1741115ca02
Thijs van der Heijden <thijsvanderheijden2@gmail.com>
2023-03-20 15:55:00 +0100

    Updates author URI.

--- duplicate-post.php
+++ duplicate-post.php
@@ -11,7 +11,7 @@
  * Description: The go-to tool for cloning posts and pages, including the powerful Rewrite & Republish feature.
  * Version:     4.5
  * Author:      Enrico Battocchi & Team Yoast
- * Author URI:  https://yoast.com
+ * Author URI:  https://yoa.st/team-yoast-duplicate
  * Text Domain: duplicate-post
  *
  * Copyright 2020-2022 Yoast BV (email : info@yoast.com)
