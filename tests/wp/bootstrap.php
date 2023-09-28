4c62e9785bd4891ed58a0dc73c0f3ba2996ddac4
Enrico Battocchi <enrico.battocchi@gmail.com>
2023-09-28 15:57:58 +0200

    Remove exclusion for the `PrefixAllGlobals` sniff after changes to `bootstrap.php`

--- tests/wp/bootstrap.php
+++ tests/wp/bootstrap.php
@@ -20,10 +20,8 @@ echo 'Version: 1.0' . PHP_EOL . PHP_EOL;
  */
 require_once dirname( __DIR__, 2 ) . '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
 
-$_wp_tests_dir = WPIntegration\get_path_to_wp_test_dir();
-
 // Get access to tests_add_filter() function.
-require_once $_wp_tests_dir . 'includes/functions.php';
+require_once WPIntegration\get_path_to_wp_test_dir() . 'includes/functions.php';
 
 tests_add_filter(
 	'muplugins_loaded',
