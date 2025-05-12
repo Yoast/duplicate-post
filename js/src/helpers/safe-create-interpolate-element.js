d5b6a55c12de7581384bb89e0193d486b888a82e
Igor <35524806+igorschoester@users.noreply.github.com>
2025-05-12 10:11:52 +0200

    Fix type doc

--- js/src/helpers/safe-create-interpolate-element.js
+++ js/src/helpers/safe-create-interpolate-element.js
@@ -4,8 +4,8 @@ import { createInterpolateElement } from "@wordpress/element";
  * Wrapper function for `createInterpolateElement` to catch errors.
  *
  * @param {string} interpolatedString The interpolated string.
- * @param {object} conversionMap The conversion map object.
- * @returns {string} The interpolated string.
+ * @param {Object<string, JSX.Element>} conversionMap The conversion map object.
+ * @returns {JSX.Element|string} The interpolated element or string if it failed.
  */
 export const safeCreateInterpolateElement = ( interpolatedString, conversionMap ) => {
 	try {
