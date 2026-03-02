d926637f89c810f6e7a366740b0f4f4e0fc0f4c9
Enrico Battocchi <enrico@yoast.com>
2026-03-02 15:40:56 +0100

    Remove `flatten_version` utility and related logic; refactor scripts to use static filenames

--- config/webpack/paths.js
+++ config/webpack/paths.js
@@ -13,25 +13,8 @@ const entry = {
 	"duplicate-post-elementor": "./duplicate-post-elementor.js",
 };
 
-/**
- * Flattens a version for usage in a filename.
- *
- * @param {string} version The version to flatten.
- *
- * @returns {string} The flattened version.
- */
-function flattenVersionForFile( version ) {
-	const versionParts = version.split( "." );
-	if ( versionParts.length === 2 && /^\d+$/.test( versionParts[1] ) ) {
-		versionParts.push( 0 );
-	}
-
-	return versionParts.join( "" );
-}
-
 module.exports = {
 	entry,
 	jsDist: jsDistPath,
 	jsSrc: jsSrcPath,
-	flattenVersionForFile,
 };
